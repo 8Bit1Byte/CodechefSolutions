a4697bbc75ee0dd628dcc8fecd3482d7f4067d43
Exploore X <exploorex@gmail.com>
2021-05-29 20:59:04 +0530

    START01 problem solution

---
+++ Practice(Beginner)/START01.py
@@ -0,0 +1,13 @@
+'''
+    Problem Name: Number Mirror
+    Problem Code: START01
+    Problem Link: https://www.codechef.com/YTPP001/problems/START01
+    Solution Link: https://www.codechef.com/viewsolution/
+'''
+
+def solve(n):
+    return n
+
+if __name__ == '__main__':
+    n = input()
+    print(solve(n))
